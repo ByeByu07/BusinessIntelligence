2cf5aeee570e13faab9a11f72a4b4a3a6b31b147
ByeByu07 <muhammadbaharuddin08123@gmail.com>
2024-10-20 20:23:58 +0700

    feat: add new data 931 items

--- result_genre_game_steam.py
+++ result_genre_game_steam.py
@@ -1,37 +1,50 @@
-import requests
-from bs4 import BeautifulSoup
+import os
 import json
-from datetime import datetime
-import re
 import time
 from scrape import scrape_steam_game
 
-def clean_number(text):
-    return re.sub(r'[^\d,]', '', text).strip()
-
-def process_game_urls(input_file, output_file):
-    # Read URLs from input file
-    with open(input_file, 'r') as f:
-        urls = json.load(f)
-
+def process_game_urls(data_folder, output_file):
     all_game_data = []
 
-    for i, url in enumerate(urls, 1):
-        print(f"Processing game {i} of {len(urls)}: {url}")
-        game_info = scrape_steam_game(url)
-        if game_info:
-            all_game_data.append(game_info)
-        else:
-            print(f"Failed to scrape data for {url}")
-        time.sleep(2)  # Be respectful to Steam's servers
+    # List all JSON files in the specified folder
+    for filename in os.listdir(data_folder):
+        if filename.endswith('.json'):
+            input_file = os.path.join(data_folder, filename)
 
-    # Save all game data to output file
-    with open(output_file, 'w') as f:
+            # Read URLs from the input file
+            with open(input_file, 'r') as f:
+                urls = json.load(f)
+
+            for i, url in enumerate(urls, 1):
+                print(f"Processing game {i} of {len(urls)}: {url}")
+                game_info = scrape_steam_game(url)
+                if game_info:
+                    all_game_data.append(game_info)
+                else:
+                    print(f"Failed to scrape data for {url}")
+                time.sleep(2)  # Be respectful to Steam's servers
+
+    current_time = time.strftime("%H_%M_%d_%m_%Y")
+
+    file_name = f"data_{current_time}.json"
+
+    if not os.path.exists(output_file):
+        os.makedirs(output_file)
+
+    file_path = os.path.join(output_file, file_name)
+
+    with open(file_path, 'w') as f:
         json.dump(all_game_data, f, indent=2)
 
-    print(f"Scraped data for {len(all_game_data)} games. Data saved to {output_file}")
+    print(f"Scraped data for {len(all_game_data)} games. Data saved to {file_path}")
+
+
+
+
+
+# Define the data folder and output file
+data_folder = "urls"  # Folder containing the input JSON files
+output_file = "datas"
 
 # Process the games
-input_file = "urls/url_19_00_19_10_24.json"
-output_file = "indie_games_data.json"
-process_game_urls(input_file, output_file)
+process_game_urls(data_folder, output_file)
